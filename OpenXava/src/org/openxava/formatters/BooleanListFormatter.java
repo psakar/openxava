7758708ea668a885432455b2c2668109de6ae522
javierpaniza <javierpaniza@openxava.org>
2016-01-21 13:36:47 +0000

    Icon instead of image for boolean values in list and collections

--- OpenXava/src/org/openxava/formatters/BooleanListFormatter.java
+++ OpenXava/src/org/openxava/formatters/BooleanListFormatter.java
@@ -29,7 +29,7 @@ public class BooleanListFormatter implements IFormatter {
 		}
 		String yes = Labels.get("yes", Locales.getCurrent());		
 		String no = Labels.get("no", Locales.getCurrent()); 
-		return r?"<div style='display: inline-block; *display: inline; width: 100%; text-align: center;'><img border='0' align='absmiddle' style='margin-right: 10px;' src='" + request.getContextPath() + "/xava/images/checked.gif'><span style='display: none;'>" + yes + "</span></div>":"<span style='display: none;'>" + no + "</span>";
+		return r?"<div style='display: inline-block; width: 100%; text-align: center;'><i class='mdi mdi-check'></i><span style='display: none;'></div>" + yes + "</span>":"<span style='display: none;'>" + no + "</span>";
 	}
 	
 	public Object parse(HttpServletRequest request, String string) {
