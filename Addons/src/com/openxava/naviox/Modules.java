a30201c4a664b42628616c4491a667f47e10c3bb
javierpaniza <javierpaniza@openxava.org>
2017-05-11 19:19:44 +0000

    New controllers for using in XavaPro

--- Addons/src/com/openxava/naviox/Modules.java
+++ Addons/src/com/openxava/naviox/Modules.java
@@ -12,7 +12,6 @@ import org.openxava.application.meta.*;
 import org.openxava.util.*;
 
 import com.openxava.naviox.impl.*;
-import com.openxava.naviox.model.*;
 import com.openxava.naviox.util.*;
 
 /**
